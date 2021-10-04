e98c3b5fe86893114e679464c7e5064cf1c1da7a
mathyingulu1 <90633640+mathyingulu1@users.noreply.github.com>
2021-10-04 19:01:23 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
-// WEB303 Assignment 2
+// WEB303 Assignment 3
 // Mathy Ingulu
 $(function () {
 
-var getJsonReturnValue = $.getJSON("test.json").done(function (data) {
-	consoel.log("The data contained"; data);
-	$.each(data.events, function (index, value) {
-	$(`#events${index}`).html(`
-	`).slideDown(3000);
-	});
-});
-
-$(document).ready(function(){
-    $("a#vprospect").click(function(){
-        $("#solution").hide();
-        console.log("is click working?");
-        $("#solution").load("prospect.html").slideDown("slow");
-
-    });
-	console.log("getjson return value is", getJsonReturnValue);
 	
-$( "#clickme" ).click(function() {
-  $( "#book" ).animate({
-   function() {
-    // Animation complete.
-  });
-});
-
-
+	$.getJSON("team.json").done(function (data) {
+		console.log("The data contained", data);
+		$.each(data.teammembers, function (index, value) {
+			console.log("Value at index " + index + " is " + value.name);
+			$(`div#team`).html(`<h3>${value.name}</h3>`, `<h4>${value.title)</h4>`, `<p>${value.bio}</p>`);
+			
+			$(`div#team`)append(newData); newData = '';
+			
+			});
+		});
+	}
+	
+	function calling_ajax() {
+		
+		$.ajax({
+			url: 'team.json', type: 'GET',
+			
+			beforeSend: function() {
+				$(`div#team`).html("<h5>Displaying ..</h5>");
+			},
+			
+			error: function() { alert("Error...!"); },
+			
+			timeout: 5000,
+			
+			success: function(data){
+				var newData = '';
+				$.each(data.teammembers, function (index, value) {
+					console.log('is it working?');
+				    newData+=`<h3>${value.name}</h3>`;
+				    newData+=`<h4>'+value.title+'</h4>`;
+				    newData+='<p>'+value.bio+'</p>';
+				    $('div#team').append(newData);
+					newData = '';
+				});
+			},
+			
+			complete: function() {
+				$('h5').css('display','none');
+			}
+		});
+	};
+	
+	$function() {
+		calling_ajax();
+		
+	});
