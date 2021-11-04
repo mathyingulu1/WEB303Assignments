9b37d597d3441fa79e490a4496eaf2c970c5564f
mathyingulu1 <90633640+mathyingulu1@users.noreply.github.com>
2021-11-03 21:37:22 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,46 @@
-// WEB303 Assignment 3
-// Mathy Ingulu
-$(function () {
+/*
+    Assignment 5
+    Mathy Ingulu
+*/
 
-	
-	$.getJSON("team.json").done(function (data) {
-		console.log("The data contained", data);
-		$.each(data.teammembers, function (index, value) {
-			console.log("Value at index " + index + " is " + value.name);
-			$(`div#team`).html(`<h3>${value.name}</h3>`, `<h4>${value.title)</h4>`, `<p>${value.bio}</p>`);
-			
-			$(`div#team`)append(newData); newData = '';
-			
-			});
-		});
-	}
-	
-	function calling_ajax() {
-		
-		$.ajax({
-			url: 'team.json', type: 'GET',
-			
-			beforeSend: function() {
-				$(`div#team`).html("<h5>Displaying ..</h5>");
-			},
-			
-			error: function() { alert("Error...!"); },
-			
-			timeout: 5000,
-			
-			success: function(data){
-				var newData = '';
-				$.each(data.teammembers, function (index, value) {
-					console.log('is it working?');
-				    newData+=`<h3>${value.name}</h3>`;
-				    newData+=`<h4>'+value.title+'</h4>`;
-				    newData+='<p>'+value.bio+'</p>';
-				    $('div#team').append(newData);
-					newData = '';
-				});
-			},
-			
-			complete: function() {
-				$('h5').css('display','none');
-			}
-		});
-	};
-	
-	$function() {
-		calling_ajax();
-		
-	});
+$(document).ready(function(){
+    const ContentCard = {
+        id: Johnson,
+        title: intertitle,
+        description: cardboard,
+        type: Master,
+    };
+
+    constructor(id, title, description, type) ;{
+        this.id = id;
+        this.title = title;
+        this.description = this.description;
+        this.type = this.type;
+    }
+
+    for (const property in ContentCard) {
+        if (property === "title") {
+            console.log(`Property name: ${property}`);
+            console.log(`Property value: ${ContentCard[property]}`);
+        }
+    }
+
+    $.toString(data.ContentCard, function (index, value) {
+        console.log("Value at index " + index + " is " + value.id);
+        $(`div#content-0`).html(`<h4>${value.title}</h4>`, `<p>${value.description}</p>`, 
+        `<div>${value.category}</div>`);
+
+    });
+});
+
+console.log(ContentCard.id);
+console.log(ContentCard["id"]);
+
+const ContentCard = [
+    "Standard",
+    "Unique",
+    "Professional",
+    "Business",
+    "Premium"
+];
+
